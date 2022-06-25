53e1adc1f856e11ba7ff94908c96b8850ae52945
王tm富贵 <11209832+wang-tms-wealth@user.noreply.gitee.com>
2022-06-25 11:35:22 +0800

    新

--- project1/js/checkLogin.js
+++
@@ -1,9 +0,0 @@
-checkLogin();
-function checkLogin() {
-    let login = getCookie("login");
-    if (login != 1) {
-        window.alert("先登录!");
-        location.href = "../html/login.html"
-        return;
-    }
-}
